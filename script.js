e7562d758e553dc680fb741f7f43cc61e06f8895
Cristian Arboleda <cristianarboledagarzon@gmail.com>
2024-10-08 22:15:28 -0500

    se agrego una alerta al encender el bombillo un numero de veces

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function bombillo_estado(){
-    interruptor = document.getElementById('interruptor');
-    bombillo = document.getElementById('bombillo');
-    console.log(bombillo);
+    var interruptor = document.getElementById('interruptor');
+    var bombillo = document.getElementById('bombillo');
     if (interruptor.innerText == 'Apagar'){
-        bombillo.src = 'images/apagado.jpg'
-        interruptor.innerText = 'Encender'
+        bombillo.src = 'images/apagado.jpg';
+        interruptor.innerText = 'Encender';
     } else {
-        bombillo.src = 'images/encendido.jpg'
-        interruptor.innerText = 'Apagar'
+        bombillo.src = 'images/encendido.jpg';
+        interruptor.innerText = 'Apagar';
+        contador += 1;
+        console.log(contador)
+        if (contador % 5 == 0){
+            alert(`Ya has encendido el bombillo ${contador} veces. Recuerda ahorrar energia!`);
+        }
     }
 }
+
+contador = 0
